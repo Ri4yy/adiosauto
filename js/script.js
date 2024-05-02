7e996f8f2ab1802dc6dbe4586b5c0e25182ff38e
Ri4yy <maksimov.1913@list.ru>
2024-05-02 18:06:52 +0300

    upd

--- js/script.js
+++ js/script.js
@@ -3,4 +3,24 @@ document.addEventListener('DOMContentLoaded' , function() {
         $(e.target).closest('.b-faq-list__item').find('.b-faq__bottom').toggleClass('open')
         $(e.target).find('.b-faq-list__btn').toggleClass('open')
     })
+
+    $('.mobile-list__btn').click((e) => {
+        $(e.target).closest('.mobile-list__item').toggleClass('open')
+    })
+
+    $('#openMenu, .overlay').click((e) => {
+        $('.mobile-menu').toggleClass('open')
+        $('.overlay').toggleClass('open')
+        $('body').toggleClass('no-scroll')
+    })
+
+    $('.close-modal').click((e) => {
+        $('.modal').removeClass('active')
+    })
+    $(document).keydown(function(e) {
+		if (e.keyCode === 27) {
+			e.stopPropagation();
+			$('.modal').removeClass('active')
+		}
+	});
 });
