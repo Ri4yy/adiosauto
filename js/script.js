5b8e120d66196189c124837037b7b17449bf5cf7
Ri4yy <maksimov.1913@list.ru>
2024-05-06 10:21:19 +0300

    add new page

--- js/script.js
+++ js/script.js
@@ -23,4 +23,69 @@ document.addEventListener('DOMContentLoaded' , function() {
 			$('.modal').removeClass('active')
 		}
 	});
+
+    $('.btn--gray').click((e) => {
+        $(e.target).closest('.b-info').find('.b-info__wrapper').toggleClass('active')
+    })
+    
+
+
+    let containerMark = document.querySelector('.brand-container'),
+    containerModel = document.querySelector('.model-container');
+    let data;
+
+fetch('/js/api.json')
+.then((response) => {
+    return response.json();
+})
+.then((responseData) => {
+    data = responseData;
+    console.log(data)
+    data.forEach(el => {
+        getMarks(el.id)
+    });
+});
+
+containerMark.addEventListener('change', (e) => {
+    clearModels();
+
+    let marks = e.target.value;
+
+    let selectedMark = data.find(el => el.id === marks);
+
+    if (selectedMark) {
+        getModels(selectedMark.models);
+    }
+})
+
+
+function getMarks(mark) {
+
+    // $('.brand-container').append(`
+    // <div class="toggle toggle--1">
+    //     <input id="brand-${mark}" type="radio" name="brand" value="${mark}">
+    //     <label for="brand-${mark}"><img src="../img/logo/audi.png" alt=""></label>
+    // </div>
+    // `);
+}
+
+function getModels(models) {
+    models.forEach((el) => {
+        let option = document.createElement("option");
+        option.value = el.name;
+        option.innerHTML = el.name;
+        containerModel.append(option);
+    })
+}
+
+function clearModels() {
+    while (containerModel.options.length > 0) {
+        containerModel.remove(0);
+    }
+
+    let initialOption = document.createElement("option");
+    initialOption.value = "";
+    initialOption.innerHTML = "Выберите модель авто";
+    containerModel.appendChild(initialOption);
+}
 });
